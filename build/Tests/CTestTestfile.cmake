dc908ae73b010a6bf0710d37b453382aa36254e0
Vlad <Meshkov Vladislav>
2022-12-26 15:56:05 +0300

    rm build

--- build/Tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vlad/FormalkiPrac/Tests
-# Build directory: /home/vlad/FormalkiPrac/build/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
--- build/Tests/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vlad/FormalkiPrac/Tests/lib/googletest/googletest
-# Build directory: /home/vlad/FormalkiPrac/build/Tests/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
